90d1f7328dcd5f8504b5d0a756f2c84e5167c0f7
Sultan <sultan.toktomambetov.04@bk.ru>
2020-09-26 11:46:00 +0600

    did layout block.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,54 @@
+import {
+  AppBar,
+  Button,
+  Container,
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
+import React from "react";
+import { Link} from "react-router-dom";
+
+const useStyle = makeStyles((theme) => ({
+  Toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  Link: {
+    color: "#fff",
+    background: green[500],
+    padding: theme.spacing(1, 2),
+    borderRadius: theme.spacing(1),
+    textDecoration: "none",
+  },
+}));
+
+const Layout = ({ children }) => {
+  const classes = useStyle();
+
+  return (
+    <div>
+      <header>
+        <AppBar position="static">
+          <Container>
+            <Toolbar className={classes.Toolbar}>
+              <Typography variant="h5" noWrap>
+                Contacts
+              </Typography>
+              <Link exact to="/add" className={classes.Link}>
+                <Typography variant="h6">add new contact</Typography>
+              </Link>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Layout from "../../components/Layout/Layout";
 import "./App.css";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/" />
+          <Route path="/add" />
+        </Switch>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
